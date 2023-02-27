3b42a0e8fc22ed2bf3bbe7df3f2f6febbfecac57
MashaZherebtsova <113789649+MashaZherebtsova@users.noreply.github.com>
2023-02-27 20:33:00 +0200

    1111

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,36 @@
 // Add imports above this line
+
+import SimpleLightbox from 'simplelightbox';
+
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+//console.log(galleryItems);
+const galleryRef =document.querySelector('.gallery');
+const markup = createMarkup(galleryItems);
+
+galleryRef.innerHTML=markup;
+
+function createMarkup(images) {
+return images.map(({preview,original,description})=>{
+    return `
+    <a class="gallery__link" href = "${original}" >
+      <img
+        class= "gallery__image"
+        src= "${preview}"
+        alt= "${description}"
+        title= "Beautiful Image"
+      />
+    </a>`
+    ;
+    
+}).join('');
+}
+new SimpleLightbox(".gallery a", {
+    /* options */
+    captionPosition: "bottom",
+    captionDelay: "250",
+    captionsData: "alt",
+    
+  });
