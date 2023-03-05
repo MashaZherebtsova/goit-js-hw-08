55e34cebcdab2e668375b51ff24b040c43c8e822
MashaZherebtsova <113789649+MashaZherebtsova@users.noreply.github.com>
2023-03-05 10:29:38 +0200

    дз

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,19 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+console.log(player);
+
+function onProgress ({seconds}){
+  localStorage.setItem("vimeoplayer-current-time",seconds);
+}
+
+player.on('timeupdate', throttle(onProgress,1000));
+
+const storedTime=localStorage.getItem("vimeoplayer-current-time");
+if (storedTime) {
+  player.setCurrentTime(storedTime);
+}
+
 
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,45 @@
+import throttle from 'lodash.throttle';
 
+const STORAGE_KEY = 'feedback-form-state';
+let formData = {};
+
+
+const refs = {
+  formEl: document.querySelector('.feedback-form'),
+  emailInputEl: document.querySelector('.feedback-form input'),
+  textareaInputEl: document.querySelector('.feedback-form textarea'),
+};
+
+refs.formEl.addEventListener('submit', onFormSubmit);
+refs.formEl.addEventListener('input', throttle(onSavingFormInput, 500));
+
+onFormRecovery();
+
+function onFormSubmit(e) {
+  e.preventDefault();
+  const emailValue = refs.emailInputEl.value;
+  const textareaValue = refs.textareaInputEl.value;
+  if (emailValue === '' || textareaValue === '') {
+    alert('Заповніть поля');
+  }
+  e.target.reset();
+  localStorage.removeItem(STORAGE_KEY);
+}
+
+function onSavingFormInput(e) {
+  formData[e.target.name] = e.target.value;
+
+  const inputValueString = JSON.stringify(formData);
+
+  localStorage.setItem(STORAGE_KEY, inputValueString);
+}
+
+function onFormRecovery() {
+  const savedValue = localStorage.getItem(STORAGE_KEY);
+
+  if (savedValue) {
+    const recoveryValue = JSON.parse(savedValue);
+    refs.emailInputEl.value = recoveryValue.email;
+    refs.textareaInputEl.value = recoveryValue.message;
+  }
+}
